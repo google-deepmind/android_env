a687179491d8817458ca726e3ab1d520d9dae6da
Anita Gergely <agergely@google.com>
2021-04-29 14:37:34 -0400

    Adding GymInterfaceWrapper to wrappers package.

--- android_env/wrappers/__init__.py
+++ android_env/wrappers/__init__.py
@@ -18,9 +18,11 @@
 from android_env.wrappers import discrete_action_wrapper
 from android_env.wrappers import flat_interface_wrapper
 from android_env.wrappers import float_pixels_wrapper
+from android_env.wrappers import gym_wrapper
 from android_env.wrappers import image_rescale_wrapper
 
 DiscreteActionWrapper = discrete_action_wrapper.DiscreteActionWrapper
 FlatInterfaceWrapper = flat_interface_wrapper.FlatInterfaceWrapper
 FloatPixelsWrapper = float_pixels_wrapper.FloatPixelsWrapper
+GymInterfaceWrapper = gym_wrapper.GymInterfaceWrapper
 ImageRescaleWrapper = image_rescale_wrapper.ImageRescaleWrapper
