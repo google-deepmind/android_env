ca85ea8b94d5a51725ad748d98927cb60b552ca4
a-gergely <46971330+a-gergely@users.noreply.github.com>
2021-05-17 18:57:26 -0400

    Update base_simulator_test.py

--- android_env/components/base_simulator_test.py
+++ android_env/components/base_simulator_test.py
@@ -61,7 +61,7 @@ class BaseSimulatorTest(absltest.TestCase):
     super().setUp()
     self._adb_controller = self.enter_context(
         mock.patch.object(adb_controller, 'AdbController', autospec=True))
-    tmp_dir = tempfile.mkdtemp(dir=absltest.get_default_test_tmpdir())
+    tmp_dir = tempfile.mkdtemp(dir=absltest.TEST_TMPDIR.value)
     self._simulator = FakeSimulator(
         adb_path='/my/adb',
         adb_server_port=5037,
