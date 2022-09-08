bce1cffa7d7549d4d7324fee390c7ce0dbe4b804
DeepMind <copybara-servicebot@google.com>
2022-09-08 13:06:10 -0700

    Add error codes to AndroidEnv errors.
    PiperOrigin-RevId: 473064830

--- android_env/components/errors.py
+++ android_env/components/errors.py
@@ -19,38 +19,58 @@
 class ReadObservationError(Exception):
   """When the environment is unable to obtain an observation from a simulator."""
 
+  ERROR_CODE = 1
+
 
 class CoordinatorError(Exception):
   """Error raised by the Coordinator."""
+
+  ERROR_CODE = 2
 
 
 class TooManyRestartsError(CoordinatorError):
   """The number of restarts has exceeded _MAX_RESTART_TRIES."""
 
+  ERROR_CODE = 3
+
 
 class AdbControllerError(Exception):
   """Errors that can be raised by ADBController."""
+
+  ERROR_CODE = 4
 
 
 class AdbControllerDeviceTimeoutError(AdbControllerError):
   """Raised when a device takes too long to respond."""
 
+  ERROR_CODE = 5
+
 
 class SimulatorError(Exception):
   """Errors that can be raised by a simulator."""
+
+  ERROR_CODE = 6
 
 
 class SendActionError(Exception):
   """Raised when action couldn't be sent successfully."""
 
+  ERROR_CODE = 7
+
 
 class StepCommandError(Exception):
   """Raised when setup step interpreter cannot process a command."""
+
+  ERROR_CODE = 8
 
 
 class WaitForAppScreenError(StepCommandError):
   """Raised when the wait_for_app_screen success check is not met."""
 
+  ERROR_CODE = 9
+
 
 class CheckInstallError(StepCommandError):
   """Raised when the check_install success check is not met."""
+
+  ERROR_CODE = 10
---
+++ android_env/components/errors_test.py
@@ -0,0 +1,62 @@
+# coding=utf-8
+# Copyright 2022 DeepMind Technologies Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for errors.py."""
+
+from absl.testing import absltest
+from absl.testing import parameterized
+from android_env.components import errors
+
+
+class ErrorsTest(parameterized.TestCase):
+
+  @parameterized.parameters(
+      (errors.ReadObservationError, 1),
+      (errors.CoordinatorError, 2),
+      (errors.TooManyRestartsError, 3),
+      (errors.AdbControllerError, 4),
+      (errors.AdbControllerDeviceTimeoutError, 5),
+      (errors.SimulatorError, 6),
+      (errors.SendActionError, 7),
+      (errors.StepCommandError, 8),
+      (errors.WaitForAppScreenError, 9),
+      (errors.CheckInstallError, 10),
+  )
+  def test_error_codes(self, error, expected_error_code):
+    with self.assertRaises(error) as context:
+      raise error()
+    self.assertEqual(context.exception.ERROR_CODE, expected_error_code)
+
+  def test_error_codes_unique(self):
+    error_codes = set()
+    errors_list = (
+        errors.ReadObservationError,
+        errors.CoordinatorError,
+        errors.TooManyRestartsError,
+        errors.AdbControllerError,
+        errors.AdbControllerDeviceTimeoutError,
+        errors.SimulatorError,
+        errors.SendActionError,
+        errors.StepCommandError,
+        errors.WaitForAppScreenError,
+        errors.CheckInstallError,
+    )
+    for error in errors_list:
+      self.assertNotIn(error.ERROR_CODE, error_codes)
+      error_codes.add(error.ERROR_CODE)
+
+
+if __name__ == '__main__':
+  absltest.main()
