3352b9f2cb66ae5c9dd864203722f26061d28cb6
Daniel Toyama <kenjitoyama@google.com>
2025-05-13 13:51:26 -0700

    Fix out-of-order test case in `emulator_simulator_test.py`.
    This test was flaky and it was failing depending on the order that the test
    cases ran. This change fixes it, and it makes the test more readable and
    robust.
    
    PiperOrigin-RevId: 758361822

--- android_env/components/simulators/emulator/emulator_simulator_test.py
+++ android_env/components/simulators/emulator/emulator_simulator_test.py
@@ -87,9 +87,9 @@ class EmulatorSimulatorTest(absltest.TestCase):
     simulator = emulator_simulator.EmulatorSimulator(config)
     self.assertNotEmpty(simulator.adb_device_name())
 
-  @mock.patch.object(os.path, 'exists', autospec=True, return_value=True)
-  @mock.patch.object(builtins, 'open', autospec=True)
-  def test_logfile_path(self, mock_open, unused_mock_exists):
+  def test_logfile_path(self):
+    """The log file's path should correspond to the one from the config."""
+
     config = config_classes.EmulatorConfig(
         logfile_path='fake/logfile/path',
         emulator_launcher=config_classes.EmulatorLauncherConfig(
@@ -101,11 +101,15 @@ class EmulatorSimulatorTest(absltest.TestCase):
         ),
     )
     simulator = emulator_simulator.EmulatorSimulator(config)
-    mock_open.return_value.__enter__.return_value.read.return_value = (
-        'fake_logs'.encode('utf-8'))
-    logs = simulator.get_logs()
-    mock_open.assert_called_once_with('fake/logfile/path', 'rb')
-    self.assertEqual(logs, 'fake_logs')
+
+    with mock.patch.object(
+        os.path, 'exists', autospec=True, return_value=True
+    ), mock.patch.object(builtins, 'open', autospec=True) as mock_open:
+      mock_file = mock_open.return_value.__enter__.return_value
+      mock_file.read.return_value = b'fake_logs'
+      logs = simulator.get_logs()
+      mock_open.assert_called_once_with('fake/logfile/path', 'rb')
+      self.assertEqual(logs, 'fake_logs')
 
   @mock.patch.object(portpicker, 'is_port_free', return_value=True)
   def test_grpc_port(self, unused_mock_portpicker):
